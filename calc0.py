ffb0f1ef63415c5169f0e87d026572c7fad45415
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2025-10-08 14:56:02 +0000

    LABUBU

---
+++ calc0.py
@@ -0,0 +1,16 @@
+print("---------Calculadora-------------")
+n1 = float(input("Informe um número: "))
+n2 = float
+(input("Informe um número: "))
+
+resultado = n1 + n2
+print("A soma é: ", resultado)
+
+resultado = n1 * n2
+print("A multiplicação é: ", resultado)
+
+resultado = n1 / n2
+print("A divisão é: ", resultado)
+
+resultado = n1 - n2
+print("A subtração é: ", resultado )
